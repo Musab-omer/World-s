87a3a00153b6ddb2dfe306e955f44c2ae320fa1f
Musab <mosab.katry@gmail.com>
2022-11-11 21:59:35 +0530

    Add Country Model
    Add Country Model to the models folder

---
+++ World/Models/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace World.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string EName { get; set; }
+        public string AName { get; set; }
+        public char Code { get; set; }
+        public int  RegionId { get; set; }
+        public Region Region { get; set; }
+    }
+}
