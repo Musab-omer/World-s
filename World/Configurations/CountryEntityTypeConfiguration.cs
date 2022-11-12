3d1963c190f020b30caade9b8be89b76fe7a2558
Musab <mosab.katry@gmail.com>
2022-11-12 23:33:04 +0530

    add notation
    add hasColumnName data notation to the EName preoperty to be EnglishName and AName property to be ArabicName in Countries table in the database

--- World/Configurations/CountryEntityTypeConfiguration.cs
+++ World/Configurations/CountryEntityTypeConfiguration.cs
@@ -14,8 +14,10 @@ namespace World.Configurations
         public void Configure(EntityTypeBuilder<Country> builder)
         {
             builder.Property(c => c.EName).HasMaxLength(30)
+                .HasColumnName("EnglishName")
                 .IsRequired(true);
             builder.Property(c => c.AName).HasMaxLength(30)
+                .HasColumnName("ArabicName")
                 .IsRequired(false);
             builder.Property(c => c.Code).HasMaxLength(10)
                 .IsRequired(true);
