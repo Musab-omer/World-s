d81a2161b25aece8ebdf927cea794a553b5ba4d5
Musab <mosab.katry@gmail.com>
2022-11-19 20:49:27 +0530

    Added State Model
    added State model to handel all states over the country

---
+++ World/Models/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace World.Models
+{
+    public class State
+    {
+        public int Id { get; set; }
+        public string EName { get; set; }
+        public string AName { get; set; }
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+        public List<City> Cities { get; set; }
+    }
+}
