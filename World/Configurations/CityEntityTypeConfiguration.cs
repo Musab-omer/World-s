6bc3affa42120c1e838a78229621a705ae1d4dbe
Musab <mosab.katry@gmail.com>
2022-11-19 21:32:13 +0530

    City Entity Configuration
    Added city entity type configuration file to configurte city model to the applictaion dbcontect

---
+++ World/Configurations/CityEntityTypeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using World.Models;
+
+namespace World.Configurations
+{
+    public class CityEntityTypeConfiguration : IEntityTypeConfiguration<City>
+    {
+        public void Configure(EntityTypeBuilder<City> builder)
+        {
+            builder.Property(c => c.EName)
+                .HasColumnName("EnglishName")
+                .HasMaxLength(30)
+                .IsRequired(true);
+            builder.Property(c => c.AName)
+                .HasColumnName("ArabicName")
+                .HasMaxLength(30)
+                .IsRequired(true);
+        }
+    }
+}
