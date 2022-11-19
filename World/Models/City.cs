e48ef44b5d4e98198ecddc4e2f1fe57305d5531d
Musab <mosab.katry@gmail.com>
2022-11-19 20:50:38 +0530

    Added City Model
    Added City model to handel all cites over state in specific country

---
+++ World/Models/City.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace World.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string EName { get; set; }
+        public string AName { get; set; }
+        public int StateId { get; set; }
+        public State State { get; set; }
+    }
+}
