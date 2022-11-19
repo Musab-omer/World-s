3f93a163db78ea4c90ef7527baeadf84e9fb9cda
Musab <mosab.katry@gmail.com>
2022-11-19 21:31:12 +0530

    State Entity Configuration
    Add state entity type configuration to configure
    state model to the appliction dbcontext

---
+++ World/Configurations/StateEntityTypeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using World.Models;
+
+namespace World.Configurations
+{
+    public class StateEntityTypeConfiguration : IEntityTypeConfiguration<State>
+    {
+        public void Configure(EntityTypeBuilder<State> builder)
+        {
+            builder.HasMany(s => s.Cities)
+                .WithOne(c => c.State);
+            builder.Property(s => s.EName).HasColumnName("EnglishName")
+                .HasMaxLength(50)
+                .IsRequired(true);
+            builder.Property(s => s.AName).HasColumnName("ArabicName")
+                .HasMaxLength(50)
+                .IsRequired(false);
+        }
+    }
+}
